d0b6acb2bdbd2315644ef285c4caf353b40e0a4e
shubham-webileapps <101321655+shubham-webileapps@users.noreply.github.com>
2022-09-13 11:38:39 +0530

    1 file modified, 4 added, 4 deleted

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import StartedTimer from './components/StartedTimer';
+import StartedTimer from './components/ShowTimer/StartedTimer';
 import './style.css';
 export default function App() {
   return <StartedTimer />;
