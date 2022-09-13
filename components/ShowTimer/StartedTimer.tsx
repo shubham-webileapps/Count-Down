064f85e6733385e1a33c2407a85f9d377a4c2f50
shubham-webileapps <101321655+shubham-webileapps@users.noreply.github.com>
2022-09-13 18:54:00 +0530

    1 file modified

--- components/ShowTimer/StartedTimer.tsx
+++ components/ShowTimer/StartedTimer.tsx
@@ -1,22 +1,19 @@
 import * as React from 'react';
 import CountdownTimer from './CountdownTimer';
-import { Typography } from '@mui/material';
+import { Typography, Box, Card } from '@mui/material';
 import { useSelector } from 'react-redux';
-// import { useDispatch } from 'react-redux';
-// import { actionCreators } from '../state/index';
-// import { bindActionCreators } from 'redux';
 
 export default function StartedTimer() {
   const time = useSelector((state) => state.time);
-  // const dispatch = useDispatch();
-  // const { setTimerTime } = bindActionCreators(actionCreators, dispatch);
-  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60;
-  // const time = 1;
 
   return (
-    <Typography variant="div">
-      <Typography variant="h5">Custom Timer !</Typography>
-      <CountdownTimer targetDate={parseInt(time)} />
-    </Typography>
+    <Box sx={{ flexGrow: 1 }}>
+      <Card container="true" spacing={2} sx={{ p: 3 }}>
+        <Typography variant="div">
+          <Typography variant="h5">Custom Timer !</Typography>
+          <CountdownTimer targetDate={parseInt(time)} />
+        </Typography>
+      </Card>
+    </Box>
   );
 }
