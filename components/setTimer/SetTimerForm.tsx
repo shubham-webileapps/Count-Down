f3e58288d9f1475375c27627ae876e1e9818bcbb
shubham-webileapps <101321655+shubham-webileapps@users.noreply.github.com>
2022-09-14 15:04:00 +0530

    1 file modified

--- components/setTimer/SetTimerForm.tsx
+++ components/setTimer/SetTimerForm.tsx
@@ -1,31 +1,26 @@
 import React from 'react';
 
-import {
-  Box,
-  // CardContent,
-  Card,
-  Typography,
-} from '@mui/material';
+import { Box, CardContent, Card, Typography } from '@mui/material';
 import TimerForm from './TimerForm';
-// import TimerFormOld from './TimerFormOld';
+import TimerFormOld from './TimerFormOld';
 const SetTimerForm = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Card container="true" spacing={2}>
-        {/* <CardContent> */}
-        {/* <Typography>
-            <Typography gutterBottom variant="h5" sx={{pt:3}}>
+        <CardContent>
+          <Typography>
+            <Typography gutterBottom variant="h5" sx={{ pt: 3 }}>
               Timer Form With Formik
             </Typography>
             <TimerFormOld />
-          </Typography> */}
-        <Typography>
-          <Typography gutterBottom variant="h5" sx={{ pt: 4 }}>
-            Timer Form
           </Typography>
-          <TimerForm />
-        </Typography>
-        {/* </CardContent> */}
+          <Typography>
+            <Typography gutterBottom variant="h5" sx={{ pt: 4 }}>
+              Timer Form
+            </Typography>
+            <TimerForm />
+          </Typography>
+        </CardContent>
       </Card>
     </Box>
   );
